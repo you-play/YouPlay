e3f0c5daba88bfc12647533d615f9e3a48251c9a
sebastian-nunez <02snunez@gmail.com>
2024-03-16 19:05:22 -0400

    refactor: auth view model

--- ios-app/YouPlay/Core/Auth/ViewModel/AuthViewModel.swift
+++ ios-app/YouPlay/Core/Auth/ViewModel/AuthViewModel.swift
@@ -9,6 +9,7 @@ import Combine
 import Foundation
 import SwiftUI
 
+@MainActor
 class AuthViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
@@ -16,10 +17,17 @@ class AuthViewModel: ObservableObject {
     func login() async throws {
         print("DEBUG: attemping to login user with email \(email)")
         try await AuthServiceImpl.shared.login(email: email, password: password)
+        reset()
     }
 
     func signUp() async throws {
         print("DEBUG: attemping to create user with email \(email)")
         try await AuthServiceImpl.shared.createUser(email: email, password: password)
+        reset()
+    }
+
+    private func reset() {
+        email = ""
+        password = ""
     }
 }
