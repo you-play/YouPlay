9ad1be35319f1af777275232459e99b5973c4d30
sebastian-nunez <02snunez@gmail.com>
2024-03-16 23:54:20 -0400

    feat: adds reset password view

--- ios-app/YouPlay/Core/Auth/View/ResetPasswordView.swift
+++ ios-app/YouPlay/Core/Auth/View/ResetPasswordView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ResetPasswordView: View {
+    @ObservedObject var viewModel: AuthViewModel
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Spacer()
+
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Reset password")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                    Text("Reset your password by entering your email address")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                }
+
+                Spacer()
+            }
+            .padding(.horizontal)
+
+            VStack(spacing: 16) {
+                TextField("Enter your email", text: $viewModel.email)
+                    .padding(15)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+
+                Button {
+                    Task {
+                        try await viewModel.resetPassword()
+                        viewModel.reset()
+                        dismiss()
+                    }
+                } label: {
+                    Text("Send")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity, minHeight: 45)
+                        .background(.green)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    ResetPasswordView()
+    ResetPasswordView(viewModel: AuthViewModel())
 }
