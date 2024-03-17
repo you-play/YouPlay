3d56a1aaf4437c4e9fbf8a0e75a271c34f0c60ea
sebastian-nunez <02snunez@gmail.com>
2024-03-16 23:54:06 -0400

    fix: fixes name

---
+++ ios-app/YouPlay/Core/Auth/View/ResetPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordView.swift
+//  YouPlay
+//
+//  Created by Sebastian on 3/16/24.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResetPasswordView()
+}
