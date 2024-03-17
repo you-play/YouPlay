b6845988034f6f1e9f86309dd02d988920f505a2
sebastian-nunez <02snunez@gmail.com>
2024-03-16 22:34:25 -0400

    feat: adds `StorageService` with Firebase Storage integration to upload profile images

---
+++ ios-app/YouPlay/Services/StorageService/StorageServiceImpl/StorageServiceImpl.swift
@@ -0,0 +1,55 @@
+//
+//  StorageServiceImpl.swift
+//  YouPlay
+//
+//  Created by Sebastian on 3/16/24.
+//
+
+import FirebaseStorage
+import Foundation
+
+/// An Singleton implementation of the `StorageService` based around `Firebase Storage`
+class StorageServiceImpl: StorageService {
+    private init() {}
+    static let shared = StorageServiceImpl()
+
+    private let storageRef = Storage.storage().reference()
+
+    /// Uploads an image and returns the public link (URL) to access the uploaded file.
+    ///
+    /// Images are uploaded to a specified `bucket` in Firebase Storage with the given `fileName`
+    /// (without an extension) and the `imageData`.
+    func uploadImage(bucket: StorageBuckets, fileName: String, imageData: Data, fileExtension: ImageFileExtension = .jpg)
+        async throws -> String
+    {
+        let fileNameWithExtension = "\(fileName)\(fileExtension.rawValue)"
+        let imageRef = storageRef.child("\(bucket.rawValue)/\(fileNameWithExtension)")
+
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/\(fileExtension.fullName)"
+
+        do {
+            imageRef.putData(imageData, metadata: metadata)
+            print("DEBUG: successfully uploaded image with name \(fileNameWithExtension)")
+
+            let downloadURL = try await imageRef.downloadURL()
+            print("DEBUG: image download URL for filename \(fileNameWithExtension): \(downloadURL.absoluteString)")
+            return downloadURL.absoluteString
+        } catch {
+            print("DEBUG: error uploading image with name \(fileNameWithExtension): \(error.localizedDescription)")
+            throw error
+        }
+    }
+}
+
+enum ImageFileExtension: String {
+    case jpg = ".jpg"
+    case png = ".png"
+
+    var fullName: String {
+        switch self {
+        case .jpg: return "jpeg"
+        case .png: return "png"
+        }
+    }
+}
