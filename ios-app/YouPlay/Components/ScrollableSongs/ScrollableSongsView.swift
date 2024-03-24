037a75004a9baa1740a5b6cc7689aba7fcc48da4
sebastian-nunez <02snunez@gmail.com>
2024-03-23 23:13:06 -0400

    feat: adds minor UI changes and code refactoring

--- ios-app/YouPlay/Components/ScrollableSongs/ScrollableSongsView.swift
+++ ios-app/YouPlay/Components/ScrollableSongs/ScrollableSongsView.swift
@@ -8,50 +8,52 @@
 import SwiftUI
 
 struct ScrollableSongsView: View {
-    let playlistTitle: String
+    let title: String
     let songs: [Song]
+
     var body: some View {
-        VStack(alignment:.leading){
-            Text("Recommended")
+        VStack(alignment: .leading, spacing: 3) {
+            Text(title)
                 .font(.headline)
-                .padding(.leading,15)
-                .padding(.top,5)
-            ScrollView(.horizontal,showsIndicators: false){
-                HStack(spacing: 20){
-                    ForEach(songs){song in
-                        VStack(alignment: .leading){
-                            Image(song.imageName)
+                .fontWeight(.bold)
+                .padding(.leading, 16)
+                .padding(.top, 5)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 20) {
+                    ForEach(songs) { song in
+                        // TODO: we have to convert each of these into a Link and open the song they clicked on
+                        VStack(alignment: .leading) {
+                            Image(song.imageName) // TODO: update to open image from URL
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .frame(width: 120, height: 120)
                                 .clipped()
-                                .cornerRadius(8)
-                            
+                                .cornerRadius(6)
+                                .padding(.bottom, 5)
+
                             Text(song.title)
-                                .font(.title3)
-                                .foregroundColor(.primary)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
                                 .lineLimit(1)
-                            
-                            Text(song.artist)
+
+                            Text(song.artists.joined(separator: ", "))
                                 .font(.caption2)
-                                .foregroundColor(.secondary)
+                                .foregroundStyle(.gray)
                                 .lineLimit(1)
-                        }//Vstack
-                        .frame(width:120)
-                    }//Foreach
-                }//Hstack
-                .padding(.horizontal,15)
-            }//ScrollView
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
         }
+        .frame(maxHeight: 200)
     }
 }
 
-struct ScrollableSongsView_Previews: PreviewProvider {
-    static var previews: some View {
-        // Your existing setup code for the preview
-        let songs = [Song.mockSong, Song.mockSong, Song.mockSong]
-        let title = "Recommended"
-        ScrollableSongsView(playlistTitle: title, songs: songs)
-            .previewLayout(.sizeThatFits) // Optional: Adjusts the size of the preview to fit the content
-    }
+#Preview {
+    ScrollableSongsView(
+        title: "Recommended",
+        songs: [Song.mock, Song.mock, Song.mock, Song.mock]
+    )
 }
--- ios-app/YouPlay/Models/Song.swift
+++ ios-app/YouPlay/Models/Song.swift
@@ -5,25 +5,25 @@
 //  Created by Joshua lopes on 3/22/24.
 //
 
-import Foundation
 import FirebaseFirestoreSwift
+import Foundation
 
-struct Song: Identifiable,Codable,Hashable{
-    @DocumentID var UUID:String?
-    
+struct Song: Identifiable, Hashable {
+    @DocumentID var UUID: String?
+
     let title: String
-    let artist: String
-    let imageName : String // will probable need to change to imageURL
-    
-    var id: String{
+    let artists: [String]
+    let imageName: String // will probable need to change to imageURL
+
+    var id: String {
         return UUID ?? NSUUID().uuidString
     }
 }
 
-extension Song{
-    static let mockSong = Song(
+extension Song {
+    static let mock = Song(
         title: "Title",
-        artist: "Artist",
+        artists: ["Artist"],
         imageName: "testSpotifyImage"
-        )
+    )
 }
