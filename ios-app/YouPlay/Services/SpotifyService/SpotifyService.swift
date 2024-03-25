d574780b52165951fecd6d1b46b1e7ebf80f17a5
sebastian-nunez <02snunez@gmail.com>
2024-03-24 21:45:13 -0400

    feat: adds the `search` endpoint for Spotify
    - Users are able to search for tracks

--- ios-app/YouPlay/Services/SpotifyService/SpotifyService.swift
+++ ios-app/YouPlay/Services/SpotifyService/SpotifyService.swift
@@ -10,4 +10,5 @@ import Foundation
 /// The `SpotifyService` is in charge of all communication with the Spotify API.
 protocol SpotifyService {
     func getAccessToken() async -> String?
+    func search(text: String) async -> SpotifySearchResponse?
 }
