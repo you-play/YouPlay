b5af7de27f04cd2145a31581bbbfcc0f7a1dd53b
sebastian-nunez <02snunez@gmail.com>
2024-03-31 11:17:50 -0400

    fix: fixes overlay blocking the tab bar

--- ios-app/YouPlay/Core/Root/View/RootView.swift
+++ ios-app/YouPlay/Core/Root/View/RootView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct RootView: View {
     @StateObject private var viewModel = RootViewModel()
-
     @State private var isShowingSongDetail = false
 
     var body: some View {
@@ -18,79 +17,82 @@ struct RootView: View {
                 AuthView()
             } else {
                 NavigationStack {
-                    TabBarView()
-                        .toolbar { // Global toolbar
-                            ToolbarItem(placement: .topBarLeading) {
-                                if let user = viewModel.currentUser {
-                                    NavigationLink(
-                                        destination: ProfileView(),
-                                        label: {
-                                            CircularProfileImageView(user: user, size: .small)
-                                        }
-                                    )
-                                }
-                            }
-                        }
-                        .overlay { // Song play/pause crumbar
-                            if let song = viewModel.song,
-                               viewModel.currentUser != nil
-                            {
-                                Button(action: {
-                                    isShowingSongDetail.toggle()
-                                }) {
-                                    VStack {
-                                        Spacer()
-
-                                        HStack {
-                                            // song info
-                                            HStack(spacing: 12) {
-                                                // image
-                                                Image(song.imageName)
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fill)
-                                                    .frame(maxWidth: 42, maxHeight: 42)
-                                                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                                                    .clipped()
-
-                                                VStack(alignment: .leading) {
-                                                    // song title
-                                                    Text(song.title)
-                                                        .font(.callout)
-                                                        .fontWeight(.semibold)
-                                                        .lineLimit(1)
-
-                                                    // artist name
-                                                    Text(song.artists.joined(separator: ", "))
-                                                        .font(.caption)
-                                                        .foregroundStyle(.gray)
-                                                        .lineLimit(1)
-                                                }
+                    ZStack {
+                        TabBarView()
+                            .toolbar { // Global toolbar
+                                ToolbarItem(placement: .topBarLeading) {
+                                    if let user = viewModel.currentUser {
+                                        NavigationLink(
+                                            destination: ProfileView(),
+                                            label: {
+                                                CircularProfileImageView(user: user, size: .small)
                                             }
-
-                                            Spacer()
-
-                                            // play/pause button
-                                            Button(action: {
-                                                viewModel.isPaused.toggle()
-                                                // TODO: play/pause song
-                                                print(viewModel.isPaused ? "Pause song" : "Play song")
-                                            }) {
-                                                Image(systemName: viewModel.isPaused ? "play.circle.fill" : "pause.circle.fill")
-                                                    .foregroundColor(.primary)
-                                                    .font(.system(size: 30))
-                                            }
-                                            .padding(.trailing, 8)
-                                        }
-                                        .padding(8)
-                                        .background(Color.gray.opacity(0.2)).clipShape(RoundedRectangle(cornerRadius: 8))
-                                        .clipped()
-                                        .padding(.bottom, 60)
-                                        .padding(.horizontal, 6)
+                                        )
                                     }
                                 }
                             }
+
+                        if let song = viewModel.song,
+                           viewModel.currentUser != nil
+                        {
+                            VStack {
+                                Spacer()
+
+                                Button(action: {
+                                    isShowingSongDetail.toggle()
+                                }) {
+                                    HStack {
+                                        // song info
+                                        HStack(spacing: 12) {
+                                            // image
+                                            Image(song.imageName)
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(maxWidth: 42, maxHeight: 42)
+                                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                                                .clipped()
+
+                                            VStack(alignment: .leading) {
+                                                // song title
+                                                Text(song.title)
+                                                    .font(.callout)
+                                                    .fontWeight(.semibold)
+                                                    .lineLimit(1)
+
+                                                // artist name
+                                                Text(song.artists.joined(separator: ", "))
+                                                    .font(.caption)
+                                                    .foregroundStyle(.gray)
+                                                    .lineLimit(1)
+                                            }
+                                        }
+
+                                        Spacer()
+
+                                        // play/pause button
+                                        Button(action: {
+                                            viewModel.isPaused.toggle()
+                                            // TODO: play/pause song
+                                            print(viewModel.isPaused ? "Pause song" : "Play song")
+                                        }) {
+                                            Image(systemName: viewModel.isPaused ? "play.circle.fill" : "pause.circle.fill")
+                                                .foregroundColor(.primary)
+                                                .font(.system(size: 30))
+                                        }
+                                        .padding(.trailing, 8)
+                                    }
+                                    .padding(8)
+                                    .background(Color.gray.opacity(0.15))
+                                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                                    .padding(.horizontal, 6)
+                                }
+                                .tint(.white)
+
+                                Spacer()
+                                    .frame(height: 60)
+                            }
                         }
-                        .tint(.white)
+                    }
                 }
                 .tint(.green)
             }
