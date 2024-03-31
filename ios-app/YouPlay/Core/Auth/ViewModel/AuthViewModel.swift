ba61e009f2b94bc4a74d3abb9a317aa6cad35c73
sebastian-nunez <02snunez@gmail.com>
2024-03-31 11:28:30 -0400

    feat: adds a loading state for auth flows

--- ios-app/YouPlay/Core/Auth/ViewModel/AuthViewModel.swift
+++ ios-app/YouPlay/Core/Auth/ViewModel/AuthViewModel.swift
@@ -14,23 +14,33 @@ class AuthViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
 
+    @Published var isLoading: Bool = false
+
     func login() async throws {
+        isLoading = true
         print("DEBUG: attemping to login user with email \(email)")
         try await AuthServiceImpl.shared.login(email: email, password: password)
+        isLoading = false
     }
 
     func signUp() async throws {
+        isLoading = true
         print("DEBUG: attemping to create user with email \(email)")
         try await AuthServiceImpl.shared.createUser(email: email, password: password)
+        isLoading = false
     }
 
     func resetPassword() async throws {
+        isLoading = true
         if !ValidationUtil.isEmail(email) {
             print("DEBUG: unable to reset password with an invalid email")
+            isLoading = false
             return
         }
 
         try await AuthServiceImpl.shared.resetPassword(email: email)
+
+        isLoading = false
     }
 
     func reset() {
