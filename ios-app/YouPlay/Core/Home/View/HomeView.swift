08478c7b971b2ef72f4ff198934278ff2e5f8959
sebastian-nunez <02snunez@gmail.com>
2024-04-03 01:39:46 -0400

    feat: add top playlists to home page

--- ios-app/YouPlay/Core/Home/View/HomeView.swift
+++ ios-app/YouPlay/Core/Home/View/HomeView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct HomeView: View {
     @StateObject var viewModel = HomeViewModel()
 
+    private let gridColumns = [
+        GridItem(.flexible(), spacing: 8),
+        GridItem(.flexible(), spacing: 8),
+    ]
+
     var body: some View {
         NavigationStack {
             ScrollView(.vertical) {
                 VStack(spacing: 16) {
+                    // top playlist
+                    LazyVGrid(columns: gridColumns, spacing: 8) {
+                        ForEach(viewModel.topPlaylists) { playlist in
+                            NavigationLink {
+                                // TODO: navigate to playlist view
+                                Text("Viewing songs for: \(playlist.title)")
+                                    .navigationTitle(playlist.title)
+                                    .navigationBarTitleDisplayMode(.inline)
+                            } label: {
+                                PlaylistCardView(playlist: playlist)
+                            }
+                            .tint(.white)
+                        }
+                    }
+                    .padding(.horizontal)
+
                     ScrollableSongsView(
                         title: "Recommended",
                         songs: Song.mocks
@@ -23,6 +44,12 @@ struct HomeView: View {
                         title: "Latest",
                         songs: Song.mocks
                     )
+                }
+                .padding(.top)
+                .onAppear {
+                    Task {
+                        await viewModel.fetchTopPlaylists()
+                    }
                 }
             }
         }
