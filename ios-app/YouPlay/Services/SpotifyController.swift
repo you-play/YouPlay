0bfd2f25477e2c58d49155515afcf55215dcc337
sebastian-nunez <02snunez@gmail.com>
2024-04-06 19:30:21 -0400

    feat: add the pause/play api functionality and ability to play any song by URI

--- ios-app/YouPlay/Services/SpotifyController.swift
+++ ios-app/YouPlay/Services/SpotifyController.swift
@@ -11,6 +11,7 @@ import Combine
 import SpotifyiOS
 import SwiftUI
 
+/// This is internal workings on the controller.
 class SpotifyController: NSObject, ObservableObject {
     /// Your own Spotify client ID. Otherwise, it won't be able to authorize you.
     var spotifyClientID: String {
@@ -26,9 +27,12 @@ class SpotifyController: NSObject, ObservableObject {
 
     /// You can set this ID directly in your Spotify API dashboard: https://developer.spotify.com/dashboard
     let spotifyRedirectURL = URL(string: "spotify-ios-you-play://spotify-login-callback")!
-    
     var accessToken: String? = nil
-    var playURI = ""
+    
+    // playback controls
+    var isPaused: Bool = false
+    var playbackRestoreURI = ""
+    var playbackRestoreSeekPosition: Int = 0
     
     private var connectCancellable: AnyCancellable?
     private var disconnectCancellable: AnyCancellable?
@@ -71,10 +75,13 @@ class SpotifyController: NSObject, ObservableObject {
             print(errorDescription)
         }
     }
-    
+}
+
+/// This is the main functionality/API to interact with Spotify Player
+extension SpotifyController {
     func connect() {
         guard let _ = appRemote.connectionParameters.accessToken else {
-            appRemote.authorizeAndPlayURI("")
+            appRemote.authorizeAndPlayURI(SILENT_TRACK_URI) // don't play audio on first "login"
             return
         }
         
@@ -86,8 +93,57 @@ class SpotifyController: NSObject, ObservableObject {
             appRemote.disconnect()
         }
     }
+    
+    /// Pauses/Plays the currently playing track.
+    ///
+    /// The Spotify SDK does NOT allow us to pause a song and then later continue interacting with the
+    /// APIs without re-authorizing.
+    ///
+    /// So, to get around this, we basically have a "silent track" that plays on an infinite loop
+    /// while "paused" and we save the state (track and position) before the
+    /// pause occurred so we can later restore it once the user "plays".
+    func pauseOrPlay() {
+        // save state of the player
+        appRemote.playerAPI?.getPlayerState { [weak self] state, _ in
+            let state = state as! SPTAppRemotePlayerState
+            
+            self?.playbackRestoreURI = state.track.uri
+            self?.playbackRestoreSeekPosition = state.playbackPosition
+            print("DEBUG: saved playback seek position as \(state.playbackPosition) ms for uri \(state.track.uri)")
+        }
+        
+        if isPaused { // paused -> play
+            play(uri: playbackRestoreURI)
+            
+            // restore play position
+            appRemote.playerAPI?.seek(toPosition: playbackRestoreSeekPosition)
+            appRemote.playerAPI?.setRepeatMode(.context) // keep playing "recommended" songs
+            print("DEBUG: Resuming song with uri \(playbackRestoreURI) at \(playbackRestoreSeekPosition) ms")
+
+        } else { // play -> paused
+            play(uri: SILENT_TRACK_URI)
+            appRemote.playerAPI?.setRepeatMode(.track) // keep playing silent audio in a loop
+            print("DEBUG: Pausing song with uri \(playbackRestoreURI) with silent track with uri \(SILENT_TRACK_URI)")
+        }
+        
+        isPaused.toggle()
+    }
+    
+    /// Plays a song (or similar) given a Spotify URI.
+    func play(uri: String) {
+        isPaused = false
+        
+        appRemote.playerAPI?.play(uri, asRadio: false) { result, error in
+            if let error = error {
+                print("DEBUG: Error playing song with uri \(uri):" + error.localizedDescription)
+            } else {
+                print("DEBUG: Successfully playing track with URI \(uri)", result ?? "")
+            }
+        }
+    }
 }
 
+/// Intercepts connection state.
 extension SpotifyController: SPTAppRemoteDelegate {
     func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
         self.appRemote = appRemote
@@ -96,7 +152,6 @@ extension SpotifyController: SPTAppRemoteDelegate {
             if let error = error {
                 debugPrint(error.localizedDescription)
             }
-            
         })
     }
     
@@ -109,8 +164,9 @@ extension SpotifyController: SPTAppRemoteDelegate {
     }
 }
 
+/// Intercepts player state.
 extension SpotifyController: SPTAppRemotePlayerStateDelegate {
     func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
-        debugPrint("Track name: %@", playerState.track.name)
+        debugPrint("Track name: \(playerState.track.name)")
     }
 }
